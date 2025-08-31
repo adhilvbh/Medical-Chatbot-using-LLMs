7d18351b765c157e284aef936bcdd46ec221dcb0
Adhil Aseem <adhil@Adhils-MacBook-Air.local>
2025-08-31 15:04:08 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Adhil Aseem",
+    author_email="vbhadhil@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
