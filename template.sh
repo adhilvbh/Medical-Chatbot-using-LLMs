c73139096e123a1ff63904427b81c4f7647deaeb
Adhil Aseem <adhil@Adhils-MacBook-Air.local>
2025-08-31 14:22:53 +0530

    folder structure

---
+++ template.sh
@@ -0,0 +1,15 @@
+#create dirctories and files for a python project
+mkdir -p src
+mkdir -p research
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+
+
+
+echo "Directories and files for a python project have been created."
